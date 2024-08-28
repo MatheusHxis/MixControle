1bbb985c41f60fe9645655328c92c0d035872272
Matheus Henrique <152836672+MatheusHxis@users.noreply.github.com>
2024-08-28 10:49:00 -0300

    FORM DOS BOTÃO
    COMEÇO DOS BOTÃO

--- php/logging.php
+++ php/logging.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_SESSION['id'] = $user['id'];
             $_SESSION['name'] = $user['name'];
             // Redirecionamento para a página inicial
-            header('Location: ../Html/segundaParte.html');
+            header('Location: ../Html/segundaParte2.html');
             exit;
         }  else {
             echo "<script>
